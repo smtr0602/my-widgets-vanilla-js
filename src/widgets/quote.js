8ba8c6905268e3499998bbe485cf8c8f612ecba9
Ryo Somatani <ryo_somatani@outlook.com>
2022-10-30 22:02:03 -0700

    update: use different api for quotes

--- src/widgets/quote.js
+++ src/widgets/quote.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 const getQuote = async () => {
   const options = {
-    method: 'GET',
+    method: 'POST',
     url: process.env.QUOTE_API_URL,
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'motivational-quotes1.p.rapidapi.com',
+    },
     timeout: 5000,
   };
   return new Promise(async (resolve, reject) => {
     try {
       const { data } = await axios(options);
-      document.querySelector(
-        '.quote'
-      ).textContent = `“${data.quote}” - ${data.person}`;
+      document.querySelector('.quote').textContent = data;
       resolve();
     } catch (error) {
       console.log(error);
